b669c9cbcd76c864d7e5dee7c02e1175826e5550
Catlin Millz <cat@Catlins-MacBook-Pro.local>
2023-12-12 23:04:51 -0500

    added test files shapes and new improved index js

--- index.js
+++ index.js
@@ -1,49 +1,67 @@
-const readline = require('readline');
-const fs = require('fs');
-const svgwrite = require('svgwrite');
+const inquirer = require('inquirer');
+const fs = require("fs")
+const {
+    SVG,
+    Text,
+    Circle,
+    Triangle,
+    Square
+} = require("./lib/shapes")
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+inquirer.prompt([
+    {
+        type: 'input',
+        name: 'text',
+        message: 'Enter up to three characters: '
+    },
+    {
+        type: 'input',
+        name: 'text_color',
+        message: 'Enter text color (keyword or hex): '
+    },
+    {
+        type: 'list',
+        name: 'shape',
+        message: 'Choose a shape: ',
+        choices: ['circle', 'triangle', 'square']
+    },
+    {
+        type: 'input',
+        name: 'shape_color',
+        message: 'Enter shape color (keyword or hex): '
+    }
+])
+.then(({text, text_color, shape, shape_color}) => {
 
-function get_user_input(prompt) {
-  return new Promise((resolve) => {
-    rl.question(prompt, (answer) => {
-      resolve(answer);
-    });
-  });
+//     const template = `
+// <svg width="300" height="200">
+//     <circle cx="150" cy="100" r="80" fill="${shape_color}"/>
+//     <text x="150" y="125" font-size="60" text-anchor="middle" fill="${text_color}">${text}</text>
+// </svg>
+//     `
+//     console.log(template)
+
+let myShape;
+
+if(shape == "circle") {
+    myShape = new Circle(shape_color)
 }
 
-async function create_svg_file(text, text_color, shape, shape_color) {
-  const dwg = svgwrite.createSvgDocument('logo.svg', { width: 300, height: 200 });
-
-  dwg.text(text, 10, 30, { fill: text_color });
-
-  if (shape === 'circle') {
-    dwg.circle(150, 100, 50, { fill: shape_color });
-  } else if (shape === 'triangle') {
-    dwg.polygon([[150, 50], [100, 150], [200, 150]], { fill: shape_color });
-  } else if (shape === 'square') {
-    dwg.rect(100, 50, 100, 100, { fill: shape_color });
-  }
-
-  fs.writeFileSync('logo.svg', dwg.toSvg());
+if(shape == "triangle") {
+    myShape = new Triangle(shape_color)
 }
 
-async function main() {
-  const text = await get_user_input("Enter up to three characters: ");
-  const text_color = await get_user_input("Enter text color (keyword or hex): ");
-
-  console.log("Choose a shape: circle, triangle, square");
-  const shape = await get_user_input("Enter a shape: ");
-
-  const shape_color = await get_user_input("Enter shape color (keyword or hex): ");
-
-  create_svg_file(text, text_color, shape, shape_color);
-
-  console.log("Generated logo.svg");
-  rl.close();
+if(shape == "square") {
+    myShape = new Square(shape_color)
 }
 
-main();
+const myText = new Text(text, text_color)
+
+const mySVG = new SVG(myShape, myText);
+
+    fs.writeFile("./examples/logo.svg", mySVG.render(), (err) => {
+        if (err) throw err;
+        console.log("Generated logo.svg")
+    })
+    
+})
